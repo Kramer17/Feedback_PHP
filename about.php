ff5c2ef3b23a52bac123f5c10937c3a65d9c0668
Janson Kramer <kramer.616@osu.edu>
2023-08-23 11:11:08 -0400

    Add existing project

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php include 'inc/header.php' ?>
+    <h2>About</h2>
+
+    <p class="text-center">Janson Kramer is one of a kind. His friends all say he is "special". His mom agrees thouroughly. Loved by all and Joannes biggest hater </p>
+    </div>
+</main>
+
+
+<?php include 'inc/footer.php'; ?>
